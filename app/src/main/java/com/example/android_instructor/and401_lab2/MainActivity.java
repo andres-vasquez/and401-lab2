b5baf80d331445187cb703b7c21e50a2c8ea3b07
Android-Instructor <andres.vasquez.a@hotmail.com>
2017-09-18 21:03:50 -0400

    Agregar acciones.

--- app/src/main/java/com/example/android_instructor/and401_lab2/MainActivity.java
+++ app/src/main/java/com/example/android_instructor/and401_lab2/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.android_instructor.and401_lab2;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context context;
+    private EditText txtUsuario;
+    private EditText txtPassword;
+    private Button btnEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
+
+        txtUsuario = (EditText)findViewById(R.id.txtUsuario);
+        txtPassword = (EditText)findViewById(R.id.txtPassword);
+        btnEnviar = (Button)findViewById(R.id.btnEnviar);
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent menuIntent = new Intent(context,MenuActivity.class);
+                menuIntent.putExtra("usuario",txtUsuario.getText().toString());
+                menuIntent.putExtra("password",txtPassword.getText().toString());
+                startActivity(menuIntent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/android_instructor/and401_lab2/MenuActivity.java
+++ app/src/main/java/com/example/android_instructor/and401_lab2/MenuActivity.java
@@ -1,13 +1,56 @@
 package com.example.android_instructor.and401_lab2;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MenuActivity extends AppCompatActivity {
+    private Context context;
+
+    private String usuario;
+    private String password;
+
+    private TextView lblBienvenida;
+    private LinearLayout lyCuenta;
+    private LinearLayout lyExtracto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        context = this;
+
+        Intent intent = getIntent();
+        usuario = intent.getStringExtra("usuario");
+        password = intent.getStringExtra("password");
+
+        lblBienvenida = (TextView)findViewById(R.id.lblBienvenida);
+        lyCuenta = (LinearLayout)findViewById(R.id.lyCuenta);
+        lyExtracto = (LinearLayout)findViewById(R.id.lyExtracto);
+
+        //Rellenar el labl bienvenida
+        lblBienvenida.setText("Bienvenido: "+usuario);
+
+        lyCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cuentaIntent = new Intent(context,CuentasActivity.class);
+                cuentaIntent.putExtra("titulo","Cuentas");
+                startActivity(cuentaIntent);
+            }
+        });
+
+        lyExtracto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent extractoIntent = new Intent(context,ExtractoActivity.class);
+                extractoIntent.putExtra("titulo","Extracto");
+                startActivity(extractoIntent);
+            }
+        });
     }
 }
